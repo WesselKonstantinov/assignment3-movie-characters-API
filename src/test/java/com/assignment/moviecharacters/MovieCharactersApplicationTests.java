4a1ecb524432abaf1b9f314d98366e4def7c73bf
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-02-28 16:11:36 +0100

    Initial commit

---
+++ src/test/java/com/assignment/moviecharacters/MovieCharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.moviecharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCharactersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
