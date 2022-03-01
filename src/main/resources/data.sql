fb79356221a8ac32634ef4bafe8862276cbeaf3e
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-03-01 12:51:45 +0100

    Seed database with sample data on startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,112 @@
+INSERT INTO movie (title, genre, release_year, director, picture, trailer) VALUES (
+    'X-Men: Days of Future Past',
+    'Action,Adventure,Sci-Fi',
+    2014,
+    'Bryan Singer',
+    'https://m.media-amazon.com/images/M/MV5BZGIzNWYzN2YtMjcwYS00YjQ3LWI2NjMtOTNiYTUyYjE2MGNkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
+    'https://youtu.be/pK2zYHWDZKo'
+);
+INSERT INTO movie (title, genre, release_year, director, picture, trailer) VALUES (
+    'X-Men: First Class',
+    'Action,Adventure,Sci-Fi',
+    2011,
+    'Matthew Vaughn',
+    'https://m.media-amazon.com/images/M/MV5BMTg5OTMxNzk4Nl5BMl5BanBnXkFtZTcwOTk1MjAwNQ@@._V1_SX300.jpg',
+    'https://youtu.be/kyQKi5-k0UU'
+);
+INSERT INTO movie (title, genre, release_year, director, picture, trailer) VALUES (
+    'The Avengers',
+    'Action,Adventure,Sci-Fi',
+    2012,
+    'Joss Whedon',
+    'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+    'https://youtu.be/eOrNdBpGMv8'
+);
+INSERT INTO movie (title, genre, release_year, director, picture, trailer) VALUES (
+    'Batman: Under the Red Hood',
+    'Animation,Action,Crime',
+    2010,
+    'Brandon Vietti',
+    'https://m.media-amazon.com/images/M/MV5BNmY4ZDZjY2UtOWFiYy00MjhjLThmMjctOTQ2NjYxZGRjYmNlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg',
+    'https://youtu.be/EcFghlzjEak'
+);
+INSERT INTO movie (title, genre, release_year, director, picture, trailer) VALUES (
+    'Batman Forever',
+    'Action,Adventure',
+    1995,
+    'Joel Schumacher',
+    'https://m.media-amazon.com/images/M/MV5BNDdjYmFiYWEtYzBhZS00YTZkLWFlODgtY2I5MDE0NzZmMDljXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+    'https://youtu.be/suOgRZflPtk'
+);
+INSERT INTO movie (title, genre, release_year, director, picture, trailer) VALUES (
+    'The Princess Bride',
+    'Adventure,Family,Fantasy',
+    1987,
+    'Rob Reiner',
+    'https://m.media-amazon.com/images/M/MV5BMGM4M2Q5N2MtNThkZS00NTc1LTk1NTItNWEyZjJjNDRmNDk5XkEyXkFqcGdeQXVyMjA0MDQ0Mjc@._V1_SX300.jpg',
+    'https://youtu.be/WNNUcHRiPS8'
+);
+
+INSERT INTO franchise (name, description) VALUES (
+    'X-Men',
+    'X-Men is an American superhero film series based on the fictional superhero team of the same name, who originally appeared in a series of comic books created by Stan Lee and Jack Kirby and published by Marvel Comics.'
+);
+
+INSERT INTO franchise (name, description) VALUES (
+    'The Marvel Cinematic Universe',
+    'The Marvel Cinematic Universe (MCU) is an American media franchise and shared universe centered on a series of superhero films produced by Marvel Studios. The films are based on characters that appear in American comic books published by Marvel Comics.'
+);
+
+INSERT INTO franchise (name, description) VALUES (
+    'Batman',
+    'The Batman film franchise consists of a total of nine theatrical live-action films and two live-action serials featuring the DC Comics superhero Batman.'
+);
+
+INSERT INTO franchise (name, description) VALUES (
+    'The Princess Bride',
+    'The Princess Bride was a popular 1973 novel which was adapted into a 1987 film which became a cult classic.'
+);
+
+INSERT INTO character (full_name, gender, picture) VALUES (
+    'Cary Elwes',
+    'MALE',
+    'https://en.wikipedia.org/wiki/Cary_Elwes#/media/File:Cary_Elwes_September_2015.jpg'
+);
+
+INSERT INTO character (full_name, alias, gender, picture) VALUES (
+    'Mandel Bruce Patinkin',
+    'Mandy Patinkin',
+    'MALE',
+    'https://en.wikipedia.org/wiki/Mandy_Patinkin#/media/File:MandyPatinkin_1_13_12.JPG'
+);
+
+INSERT INTO character (full_name, gender, picture) VALUES (
+    'Val Kilmer',
+    'MALE',
+    'https://en.wikipedia.org/wiki/Val_Kilmer#/media/File:Val_Kilmer_Cannes.jpg'
+);
+
+INSERT INTO character (full_name, gender, picture) VALUES (
+    'Kelly Hu',
+    'FEMALE',
+    'https://en.wikipedia.org/wiki/Kelly_Hu#/media/File:Kelly_Hu.jpg'
+);
+
+INSERT INTO character (full_name, gender, picture) VALUES (
+    'Scarlett Johansson',
+    'FEMALE',
+    'https://en.wikipedia.org/wiki/Scarlett_Johansson#/media/File:Scarlett_Johansson_by_Gage_Skidmore_2_(cropped,_2).jpg'
+);
+
+INSERT INTO character (full_name, gender, picture) VALUES (
+    'Michael Fassbender',
+    'MALE',
+    'https://en.wikipedia.org/wiki/Michael_Fassbender#/media/File:Michael_Fassbender_by_Gage_Skidmore_2015.jpg'
+);
+
+INSERT INTO character (full_name, alias, gender, picture) VALUES (
+    'Maria Halle Berry',
+    'Halle Berry',
+    'FEMALE',
+    'https://en.wikipedia.org/wiki/Halle_Berry#/media/File:Halle_Berry_by_Gage_Skidmore_2.jpg'
+);
