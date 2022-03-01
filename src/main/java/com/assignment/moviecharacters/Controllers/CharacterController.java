02486d5cbb223a6dde0703ce555880d98100059a
Maghawry <mag.hauwry@gmail.com>
2022-03-01 22:27:07 +0100

    Adding character in controller CharacterController.

--- src/main/java/com/assignment/moviecharacters/Controllers/CharacterController.java
+++ src/main/java/com/assignment/moviecharacters/Controllers/CharacterController.java
@@ -5,18 +5,33 @@ import com.assignment.moviecharacters.Repositories.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Characters")
+@RequestMapping("/api/characters")
 public class CharacterController {
 
     @Autowired
     private CharacterRepository characterRepository;
+
+
+    //add character
+    @PostMapping("/add_character")
+    public ResponseEntity<Character> addCharacter(@RequestBody Character character) {
+        HttpStatus status;
+
+        if (character.id != null) {
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(character, status);
+        }
+        status = HttpStatus.OK;
+        character = characterRepository.save(character);
+        return new ResponseEntity<>(character, status);
+    }
 
     @GetMapping("/all") //Get all characters
     public ResponseEntity<List<Character>> getAllCharacters() {
@@ -24,5 +39,7 @@ public class CharacterController {
         List<Character> characters = characterRepository.findAll();
         return new ResponseEntity<>(characters, status);
     }
+
+
 
 }
