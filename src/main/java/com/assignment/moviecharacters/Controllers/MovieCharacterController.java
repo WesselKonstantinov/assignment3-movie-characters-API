5e537d7f16cf9ebf5adfc7b7f987a206859f781a
Wessel Konstantinov <wes_kon1@hotmail.com>
2022-03-03 13:32:39 +0100

    Create services, clean up controllers

--- src/main/java/com/assignment/moviecharacters/Controllers/MovieCharacterController.java
+++ src/main/java/com/assignment/moviecharacters/Controllers/MovieCharacterController.java
@@ -1,108 +1,42 @@
 package com.assignment.moviecharacters.Controllers;
 
 import com.assignment.moviecharacters.Models.MovieCharacter;
-import com.assignment.moviecharacters.Repositories.MovieCharacterRepository;
+import com.assignment.moviecharacters.Services.MovieCharacterService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/characters")
 public class MovieCharacterController {
 
     @Autowired
-    private MovieCharacterRepository movieCharacterRepository;
+    private MovieCharacterService movieCharacterService;
 
-    //Get all characters
     @GetMapping("/all")
     public ResponseEntity<List<MovieCharacter>> getAllMovieCharacters() {
-        HttpStatus status = HttpStatus.OK;
-        List<MovieCharacter> movieCharacters = movieCharacterRepository.findAll();
-        return new ResponseEntity<>(movieCharacters, status);
+        return movieCharacterService.getAllMovieCharacters();
     }
 
-    // Get specific character
     @GetMapping("/{id}")
     public ResponseEntity<MovieCharacter> getMovieCharacter(@PathVariable Long id) {
-        MovieCharacter movieCharacter = new MovieCharacter();
-        HttpStatus status;
-
-        if (movieCharacterRepository.existsById(id)) {
-            status = HttpStatus.OK;
-            movieCharacter = movieCharacterRepository.findById(id).get();
-        } else {
-            status = HttpStatus.NOT_FOUND;
-        }
-        return new ResponseEntity<>(movieCharacter, status);
+        return movieCharacterService.getMovieCharacter(id);
     }
 
-    //Add character
     @PostMapping("/add")
     public ResponseEntity<MovieCharacter> addMovieCharacter(@RequestBody MovieCharacter movieCharacter) {
-        HttpStatus status;
-
-        if (movieCharacter.id != null) {
-            status = HttpStatus.BAD_REQUEST;
-            return new ResponseEntity<>(movieCharacter, status);
-        }
-        status = HttpStatus.OK;
-        movieCharacter = movieCharacterRepository.save(movieCharacter);
-        return new ResponseEntity<>(movieCharacter, status);
+        return movieCharacterService.addMovieCharacter(movieCharacter);
     }
 
-    //Update character
     @PatchMapping("/{id}")
     public ResponseEntity<MovieCharacter> updateMovieCharacter(@PathVariable Long id, @RequestBody MovieCharacter newMovieCharacter) {
-        MovieCharacter movieCharacter = new MovieCharacter();
-        HttpStatus status;
-
-        if (movieCharacterRepository.existsById(id)) {
-
-            Optional<MovieCharacter> characterRepos = movieCharacterRepository.findById(id);
-            movieCharacter = characterRepos.get();
-
-            if (newMovieCharacter.fullName != null) {
-                movieCharacter.fullName = newMovieCharacter.fullName;
-            }
-            if (newMovieCharacter.alias != null) {
-                movieCharacter.alias = newMovieCharacter.alias;
-            }
-            if (newMovieCharacter.gender != null) {
-                movieCharacter.gender = newMovieCharacter.gender;
-            }
-            if (newMovieCharacter.picture != null) {
-                movieCharacter.picture = newMovieCharacter.picture;
-            }
-            if (newMovieCharacter.movies != null && !newMovieCharacter.movies.isEmpty()) {
-                movieCharacter.movies.addAll(newMovieCharacter.movies);
-            }
-            status = HttpStatus.OK;
-            movieCharacterRepository.save(movieCharacter);
-
-        } else {
-            status = HttpStatus.NOT_FOUND;
-
-        }
-        return new ResponseEntity<>(movieCharacter, status);
+        return movieCharacterService.updateMovieCharacter(id, newMovieCharacter);
     }
 
-    //Delete Character
     @DeleteMapping("/{id}")
     public ResponseEntity<MovieCharacter> deleteMovieCharacter(@PathVariable Long id) {
-        HttpStatus status;
-
-        if (movieCharacterRepository.existsById(id)) {
-            movieCharacterRepository.deleteById(id);
-            status = HttpStatus.OK;
-
-        } else {
-            status = HttpStatus.NOT_FOUND;
-        }
-        return new ResponseEntity<>(status);
-
+        return movieCharacterService.deleteMovieCharacter(id);
     }
 }
---
+++ src/main/java/com/assignment/moviecharacters/Services/FranchiseService.java
@@ -0,0 +1,105 @@
+package com.assignment.moviecharacters.Services;
+
+import com.assignment.moviecharacters.Models.Franchise;
+import com.assignment.moviecharacters.Models.Movie;
+import com.assignment.moviecharacters.Repositories.FranchiseRepository;
+import com.assignment.moviecharacters.Repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FranchiseService {
+
+    @Autowired
+    private FranchiseRepository franchiseRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    // Get all franchises
+    public ResponseEntity<List<Franchise>> getAllFranchises() {
+        HttpStatus status = HttpStatus.OK;
+        List<Franchise> franchises = franchiseRepository.findAll();
+        return new ResponseEntity<>(franchises, status);
+    }
+
+    // Get specific franchise
+    public ResponseEntity<Franchise> getFranchise(Long id) {
+        Franchise franchise = new Franchise();
+        HttpStatus status;
+
+        if (franchiseRepository.existsById(id)) {
+            status = HttpStatus.OK;
+            franchise = franchiseRepository.findById(id).get();
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(franchise, status);
+    }
+
+    // Add franchise
+    public ResponseEntity<Franchise> addFranchise(Franchise franchise) {
+        HttpStatus status;
+
+        if (franchise.id != null) {
+            status = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(franchise, status);
+        }
+        status = HttpStatus.OK;
+        franchise = franchiseRepository.save(franchise);
+        return new ResponseEntity<>(franchise, status);
+    }
+
+    // Update franchise
+    public ResponseEntity<Franchise> updateFranchise(Long id, Franchise newFranchise) {
+        Franchise franchise = new Franchise();
+        HttpStatus status;
+
+        if (franchiseRepository.existsById(id)) {
+
+            Optional<Franchise> franchiseRepos = franchiseRepository.findById(id);
+            franchise = franchiseRepos.get();
+
+            if (newFranchise.name != null) {
+                franchise.name = newFranchise.name;
+            }
+            if (newFranchise.description != null) {
+                franchise.description = newFranchise.description;
+            }
+            if (newFranchise.movies != null && !newFranchise.movies.isEmpty()) {
+                franchise.movies.addAll(newFranchise.movies);
+            }
+            status = HttpStatus.OK;
+            franchiseRepository.save(franchise);
+
+        } else {
+            status = HttpStatus.NOT_FOUND;
+
+        }
+        return new ResponseEntity<>(franchise, status);
+    }
+
+    // Delete franchise
+    public ResponseEntity<Franchise> deleteFranchise(Long id) {
+        HttpStatus status;
+
+        if (franchiseRepository.existsById(id)) {
+            Franchise franchise = franchiseRepository.findById(id).get();
+            for (Movie movie: franchise.movies) {
+                movie.franchise = null;
+                movieRepository.save(movie);
+            }
+            franchiseRepository.deleteById(id);
+            status = HttpStatus.OK;
+
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(status);
+    }
+}
